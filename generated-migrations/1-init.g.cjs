f0f5af3ba190f2ac0965042179805d2e724527da
Aleksa <aleksa.mitic5859@gmail.com>
2025-09-11 17:13:17 +0200

    init dev

--- generated-migrations/1-init.g.cjs
+++ generated-migrations/1-init.g.cjs
@@ -1,23 +1,3 @@
-#!/usr/bin/env bash
-
-echo "Setting up - This script is designed to be run from the outer repo."
-
-ln -s contentful-tools/bitbucket-pipelines-client.yml bitbucket-pipelines.yml
-
-cat > .env << EOF
-SPACE_ID=<The ID of your Contentful space (it's in the URL)>
-DEFAULT_BASE_ENV_FOR_MIGRATIONS=<The environment cloned to make your migrations (usually your lowest environment)>
-NEXT_PUBLIC_ENVIRONMENTS_PROGRESSION=<Comma separated list of your environment names in progression order e.g: test,pre-prod,production>
-PROD_ENVIRONMENT=<Your production environment name>
-EOF
-
-cat .env.local << EOF
-CMA_TOKEN=<Content management token>
-CDA_TOKEN=<Content delivery token (with access to all envs)
-EOF
-
-mkdir generated-migrations
-cat > generated-migrations/1-init.g.cjs << EOF
 function migrationFunction(migration, context) {
     const contentfulMigrationTracking = migration.createContentType("contentfulMigrationTracking");
     contentfulMigrationTracking
@@ -60,4 +40,3 @@ function migrationFunction(migration, context) {
     contentfulMigrationTracking.changeFieldControl("title", "builtin", "singleLine", { "helpText": "Please don't edit this content type, it's used to track changes to content." })
 }
 module.exports = migrationFunction;
-EOF
