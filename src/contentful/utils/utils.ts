5ce1447c9ccdcdc20e305d76069a1cd08b13cf9e
Aleksa <aleksa.mitic5859@gmail.com>
2025-09-14 17:55:44 +0200

    fixes and test migration

--- src/contentful/utils/utils.ts
+++ src/contentful/utils/utils.ts
@@ -1,4 +1,11 @@
 import {execSync} from "child_process";
 
 export const delay = (time: number) => new Promise(res=>setTimeout(res,time));
-export const rootDir = execSync("git rev-parse --show-superproject-working-tree").toString().trim()
+export const rootDir = (() => {
+    try {
+        const superProjectRoot = execSync("git rev-parse --show-superproject-working-tree").toString().trim();
+        return superProjectRoot || execSync("git rev-parse --show-toplevel").toString().trim();
+    } catch (error) {
+        return process.cwd();
+    }
+})()
